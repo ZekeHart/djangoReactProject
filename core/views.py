1246a739b3585be77939724605f90064af783067
Zeke Hart <heirloch@gmail.com>
2019-09-29 17:51:35 -0400

    adds API view to views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from core.models import Lead
+from core.serializers import LeadSerializer
+from rest_framework import generics
 
-# Create your views here.
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
